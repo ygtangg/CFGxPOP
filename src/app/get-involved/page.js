76e557619378c92d38c968c26d3d43c5c6370fdc
apease04 <124762025+apease04@users.noreply.github.com>
2024-03-05 21:00:15 -0800

    3/5 Liveshare changes
    started writing text, buttons, etc

--- src/app/get-involved/page.js
+++ src/app/get-involved/page.js
@@ -9,14 +9,16 @@ export default function Home() {
           <div class={styles.overlay}></div>
           <h1 class={styles.title}>Support Our Mission</h1>
           <div class={styles.buttonContainer}>
-            <button class={styles.button}>Donate</button>
-            <button class={styles.button}>Screening</button>
+            <a href='#donate'>
+              <button class={styles.button}>Donate</button>
+            </a>
+            <button class={styles.button} >Screening</button>
             <button class={styles.button}>Volunteer</button>
             <button class={styles.button}>Sponsor</button>
           </div>
         </div>
         <div class={styles.mainTexts}>
-          <h2 class={styles.subtitle}>Donate</h2>
+          <h2 class={styles.subtitle} id="donate">Donate</h2>
           <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
         <div class={styles.mainTexts}>
